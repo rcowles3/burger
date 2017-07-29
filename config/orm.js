0c1939003744b0845c6dca1b6a8e37c851f04787
rcowles3 <ryan.cowles3@gmail.com>
2017-07-29 00:26:27 -0700

    need to review class recordings and activites

---
+++ config/orm.js
@@ -0,0 +1,8 @@
+// Import MySQL connection.
+var connection = require("./connection.js");
+
+// ================================================
+// FUNCTIONS 
+// ================================================
+
+// function to select all data from burgers_db
