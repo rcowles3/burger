bda08d08f398b57bca582dbf93381c83ea1690e3
rcowles3 <ryan.cowles3@gmail.com>
2017-07-29 19:10:27 -0700

    JS complete, working

--- config/connection.js
+++ config/connection.js
@@ -3,20 +3,20 @@ const mysql = require("mysql");
 
 // const to hold our connection credentials, hidden in .env file
 const connection = mysql.createConnection({
-  port: 3306,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: "cat_db"
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "Fdd4e!i$f$",
+    database: "burgers_db"
 });
 
 // Make connection.
 connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
